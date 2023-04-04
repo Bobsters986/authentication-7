025de7451c0d9ec2ebe96ded2de2ec34fef09721
Bobby Luly <bobbyluly@gmail.com>
2023-04-04 10:31:29 -0600

    Complete sessions, cookies, and authorization lesson

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+    # unless cookies[:greeting]
+    #   cookies.encrypted[:greeting] = "Hello world!"
+
+    # cookies[:user_id] = current_user.id if current_user
+    # cookies.permanent[:user_id] = current_user.id if current_user
+    # cookies.delete :user_id
   end
 end
